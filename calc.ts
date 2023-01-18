3bf52e3badf2efc6274f239b1809b249b9b10dab
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-01-18 18:35:04 -0500

    Added a function

--- calc.ts
+++ calc.ts
@@ -1,1 +1,9 @@
-console.log("Hi")
+function regDeriv_num(): number {
+    let fourth_power_coeff: number = "Enter the fourth-power coefficient, type 0 if your equation has no fourth-power term: "
+    let third_power_coeff: number = "Enter the third-power coefficient, type 0 if your equation has no third-power term: "
+    let second_power_coeff: number = "Enter the second-power coefficient, type 0 if your equation has no second-power term: "
+    let first_power_coeff: number = "Enter the first-power coefficient, type 0 if your equation has no firstpower term: "
+    let constant: number = "Enter the constant term, type 0 if your equation has no constant: "
+    console.log(fourth_power_coeff*4)
+}
+regDeriv_num()
